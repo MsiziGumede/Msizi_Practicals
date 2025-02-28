dd96ed9b1e13f4eb16bf0d45bb7e33ebfec9ced5
Msizi Gumede <u24784606@tuks.co.za>
2025-02-28 11:33:36 +0200

    Msizi

---
+++ Controllers/myStudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MsiziGumedeStudyGroup.Controllers
+{
+    public class myStudentController : Controller
+    {
+        // GET: myStudent
+        public ActionResult StudentView()
+        {
+         List<Models.myStudents> students = new List<Models.myStudents>();
+
+            students.Add(new Models.myStudents { StudentNumber = "u24784606", Name = "Msizi", Surname = "Gumede", Email = "u24784606@tuks.co.za" });
+            students.Add(new Models.myStudents { StudentNumber = "u24634400", Name = "Boitshoko", Surname = "Mphahlele", Email = "u24634400@tuks.co.za" });
+            students.Add(new Models.myStudents { StudentNumber = "u24717739", Name = "Ritshidze", Surname = "Nedzimauli", Email = "u24717739@tuks.co.za" });
+            students.Add(new Models.myStudents { StudentNumber = "u24924718", Name = "Gundo", Surname = "Ndou", Email = "u24924718@tuks.co.za" });
+            students.Add(new Models.myStudents { StudentNumber = "u24768929", Name = "Lethabo", Surname = "Moses", Email = "u24768929@tuks.co.za" });
+
+          return View(students);
+        }
+    }
+}
